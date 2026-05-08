f452dfc0bd18d9d19cfe4127a53cb8a7a37c856d
AzureTear <ba.maiolatesi@duocuc.cl>
2026-05-07 20:16:19 -0400

    Created Navbar and Routes
    Creation of navbar and react with react-router-dom

--- src/components/organism/Navbar.jsx
+++ src/components/organism/Navbar.jsx
@@ -1,2 +1,28 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import Image from '../atoms/Image';
+import '../../styles/global.css';
+import Here2 from "../../assets/test1.png"
+import '../../styles/components/organism/Navbar.css'
 
+function NavBar() {
+  return (
+    <Navbar id="navbar" bg= "dark" variant="dark" expand="lg" >
+      <Container>
+        <Image src={Here2} className="round-tiny-img" />
+        <Navbar.Brand href="/">RedNorte</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Inicio</Nav.Link>
+            <Nav.Link path="/agendar-cita"> Agendar cita</Nav.Link>
+            <Nav.Link path="/crear-usuario"> Crear Usuario</Nav.Link>
+            <Nav.Link path="/iniciar-sesion"> Iniciar Session</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
+export default NavBar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap';
+
+
+function Home() {
+ return (
+   <Container className="my-5">
+     <h1>Página de Inicio</h1>
+     <p>Bienvenidos a nuestro sitio web.</p>
+   </Container>
+ );
+}
+
+export default Home;
